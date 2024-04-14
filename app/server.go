4ec1089a29c5f7a21bd3944bf036eec9033e2557
Muhless <nuryadi.muhta@gmail.com>
2024-04-14 21:55:30 +0700

    eps 8 : faker dan seeder

--- app/server.go
+++ app/server.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Muhless/GO-TOKO/databases/seeders"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
@@ -38,6 +39,7 @@ func (server *Server) Initialize(appConfig AppConfig, dbConfig DBConfig) {
 
 	server.InitializeDB(dbConfig)
 	server.initializeRoutes()
+	seeders.DBSeed(server.DB)
 }
 
 func (server *Server) InitializeDB(dbConfig DBConfig) {
