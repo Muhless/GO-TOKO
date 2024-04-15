58610b55c924f8b829e9227f95467b4413620940
Muhless <nuryadi.muhta@gmail.com>
2024-04-15 16:36:19 +0700

    eps 12 : menampilkan list produk

--- app/controllers/home_controller.go
+++ app/controllers/home_controller.go
@@ -2,11 +2,10 @@ package controllers
 
 import (
 	"net/http"
-
 	"github.com/unrolled/render"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+func(server *Server) Home(w http.ResponseWriter, r *http.Request) {
 	render := render.New(render.Options{
 		Layout: "layout",
 	})
